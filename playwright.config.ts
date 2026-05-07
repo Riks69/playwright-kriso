babe0f341c65de5092ffc954e390b9b49ca3cda4
Riks69 <144675519+Riks69@users.noreply.github.com>
2026-05-07 12:59:53 +0300

    Update Playwright config to modify retries and locale
    Reduced the number of retries from 2 to 1 and added locale setting.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,20 +2,16 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 60000,  // Suurenda 60 sekundini
-  expect: {
-    timeout: 15000  // Suurenda ootamist 15 sekundini
-  },
-  retries: 2,
+  timeout: 60000,
+  retries: 1,
   reporter: 'html',
 
   use: {
     baseURL: 'https://www.kriso.ee',
+    locale: 'et-EE',            
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
-    actionTimeout: 15000,  // Iga actioni jaoks 15 sekundit
-    navigationTimeout: 30000,  // Navigatsiooni jaoks 30 sekundit
   },
 
   projects: [
